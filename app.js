fc4b62547c3cd09a21a3f0c2570ae1d3f995c143
Dylan Hassinger <dylanhassinger@gmail.com>
2015-10-31 02:37:41 -0500

    first prototype

--- app.js
+++ app.js
@@ -1,16 +1,84 @@
+// setup
+
+	// libraries
+	var express = require("express"),
+		path = require("path"),
+		fileExists = require("exists-file"),
+		fs = require("fs");
+	
+	// site config
+	var site = {
+		"theme": "default",
+		"homepage": "index"
+	};
+	
+	// app config
+	var folders = {
+		"themes": "themes",
+		"content": "content",
+		"pages": "pages"	
+	};
+	
+	var paths = {};	
+	paths.theme = path.join(folders.themes, site.theme);
+	paths.pages = path.join(folders.content, folders.pages);
+	paths.homepage = path.join(paths.pages, site.homepage);
+	
+	// instantiate express
+	var app = express();
+	var port = process.env.PORT || 8080;
+	
+	// set up view engine
+	app.set("view engine", "ejs");	
+	app.set("views", path.join(__dirname, folders.themes, site.theme));	
+	app.use(express.static(path.join(folders.themes, site.theme)));
+
+// config
+
 // routing
 
-	var config = {
-		"name": "Demo Site",
-		"slug": "demo"
-	};
+	var router = express.Router();
+	
+	// index
+	
+		router.get("/", function(req, res) {
+		    res.render("index", {content: fileOpen(paths.homepage)});
+		});
+	
+	// other pages
+	
+		router.get("/:page", function(req, res) {
+		    res.render("index", {content: fileOpen(path.join(paths.pages, req.page))});
+		});
+		
+		router.param("page", function(req, res, next, page) {
+		    req.page = page;
+		    next(); 
+		});
+	
+	// apply routes and start the server
+	
+		app.use('/', router);		
+		app.listen(port);
+		
+// functions
 
-	if (%1) req.page = %1;
-	else req.page = "index";
+	function fileOpen(filepath) {
+	
+		var file_formats = [
+			".html",
+			".md",
+			".ejs"
+		];
+	
+		if (fileExists(filepath)) return fs.readFileSync(filepath, "utf8");
+	
+		else {
+			file_formats.forEach(function(file_format) {
+				if (fileExists(filepath + file_format)) return fs.readFileSync(filepath + file_format, "utf8");
+			});	
+		}		
 
-	if (?theme) req.theme = "theme";
-	else req.theme = config.theme;
-
-	// render partials on demand
-
-	render_theme();
+		return false;
+				
+	}
