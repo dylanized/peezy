e1119df63b142f4e353e44b377f21eb25723a4ce
Dylan Hassinger <dylanhassinger@gmail.com>
2015-11-07 13:27:15 -0600

    fixed error handling

--- app.js
+++ app.js
@@ -2,13 +2,13 @@
 
 	var peezy = require("peezy");
 	
-	var config = require("./config.json");	
-	config.dirname = __dirname;
-	
+	var config = require("./config.json");
+	config.siteFolder = "site";	
+
 // launch server
 
 	peezy.init(config, function() {
 	
-		console.log("Success!");
+		console.log("Peezy is running");
 	
 	});	
