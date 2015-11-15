853fa0d875ef3879398df7680d0ebac5af2ab055
Dylan Hassinger <dylanhassinger@gmail.com>
2015-11-15 01:09:21 -0600

    moving args into module

--- app.js
+++ app.js
@@ -3,11 +3,6 @@
 	var peezy = require("peezy");
 	
 	var config = require("./config.json");
-	
-	// siteFolder override
-	var args = process.argv.slice(2);
-	if (args[0]) config.siteFolder = args[0];
-	if (args[1]) config.themeOverride = args[1];
 
 // launch server
 
